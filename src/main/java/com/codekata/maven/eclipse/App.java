472fb05989084ca71aa67849655bd080a3d93950
Jacob Dawson <jdawson@sourceallies.com>
2021-08-12 11:45:30 -0500

    Init project

---
+++ src/main/java/com/codekata/maven/eclipse/App.java
@@ -0,0 +1,13 @@
+package com.codekata.maven.eclipse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
